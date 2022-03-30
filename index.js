c152b1e5d6f8454338b2e1b25cfed49d2012cdb6
JacobLara660 <97487821+JacobLara660@users.noreply.github.com>
2022-03-29 20:21:42 -0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 let answer  = window.prompt('Do you head left, or right?')
+
 let firstAnswer  = window.prompt('Do you head left, or right?')
 console.log(right)
+
+let firstAnswer  = window.prompt('Do you head left, or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(answer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon all together. Which do you take?`)
+
+}
+
+function start(){
+    let answer  = window.prompt('Do you head left, or right?')
+    if(answer === 'left'){
+       goLeft()
+    } else if(answer === 'right') {
+        goRight()
+    }
+}
+
+function goLeft(){
+    let answer = window.prompt(`You come across a stray cat. 
+    It scampers off down a small hole, just large enough for you to crawl through. 
+    Do you follow it, or continue on your path?`)
+    if(answer === 'back'){
+        leftOrRight()
+    }
+}
+
+function goRight(){
+    let answer = window.prompt(`You come across a snoring dragon. 
+    On the other side of him, you see a shiny chest of treasure. Another path would 
+    lead you away from the dragon all together. Which do you take?`)
+    if(answer === 'back'){
+        leftOrRight()
+    }
+}
+
